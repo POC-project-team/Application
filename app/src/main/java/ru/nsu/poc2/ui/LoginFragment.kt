5e1a1d2e16fa56aff3ce124f5a9bcf10a11b351b
L3XxXa <alexkkk@inbox.ru>
2022-12-17 21:57:44 +0700

    Login screen is Done

--- app/src/main/java/ru/nsu/poc2/ui/LoginFragment.kt
+++ app/src/main/java/ru/nsu/poc2/ui/LoginFragment.kt
@@ -8,8 +8,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import ru.nsu.poc2.R
 import ru.nsu.poc2.databinding.FragmentLoginBinding
 import ru.nsu.poc2.network.StatusValue
+import ru.nsu.poc2.utils.FieldValidators
 import ru.nsu.poc2.utils.LogTags
 import ru.nsu.poc2.viewmodels.LoginViewModel
 
@@ -36,6 +38,30 @@ class LoginFragment: Fragment(){
     }
 
     private fun login() {
+        binding.errorPassword.visibility = View.INVISIBLE
+        binding.errorEmail.visibility = View.INVISIBLE
+        if (!FieldValidators.validateFieldsNotEmpty(binding.email.text.toString())){
+            if (!FieldValidators.validateFieldsNotEmpty(binding.password.text.toString())){
+                binding.errorEmail.text = getString(R.string.enter_email)
+                binding.errorPassword.text = getString(R.string.enter_password)
+                binding.errorPassword.visibility = View.VISIBLE
+                binding.errorEmail.visibility = View.VISIBLE
+                return
+            }
+            binding.errorEmail.text = getString(R.string.enter_email)
+            binding.errorEmail.visibility = View.VISIBLE
+            return
+        }
+        if (!FieldValidators.validateFieldsNotEmpty(binding.password.text.toString())){
+            binding.errorPassword.text = getString(R.string.enter_password)
+            binding.errorPassword.visibility = View.VISIBLE
+            return
+        }
+        if (!FieldValidators.validateEmail(binding.email.text.toString())){
+            binding.errorEmail.text = getString(R.string.not_email_error)
+            binding.errorEmail.visibility = View.VISIBLE
+            return
+        }
         viewModel.login(binding.email.text.toString(), binding.password.text.toString())
     }
 
@@ -43,13 +69,15 @@ class LoginFragment: Fragment(){
         viewModel.status.observe(viewLifecycleOwner){
             when(it){
                 StatusValue.ERROR -> {
-
+                    binding.loadingBar.visibility = View.INVISIBLE
+                    Toast.makeText(context, "Error while login", Toast.LENGTH_SHORT).show()
                 }
                 StatusValue.SUCCESS->{
+                    binding.loadingBar.visibility = View.INVISIBLE
 
                 }
                 StatusValue.LOADING->{
-
+                    binding.loadingBar.visibility = View.VISIBLE
                 }
             }
         }
